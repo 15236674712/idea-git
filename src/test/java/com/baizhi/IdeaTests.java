7f4a6bf50e3e2356adf27fcb7570cd5c5a33257d
zhangcn <15010729294@163.com>
2020-12-30 10:06:54 +0800

    测试类添加一个测试类

---
+++ src/test/java/com/baizhi/IdeaTests.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaTests {
+
+    @Test
+    void contextLoads() {
+        int a = 20;
+        int b = 30;
+    }
+
+}
