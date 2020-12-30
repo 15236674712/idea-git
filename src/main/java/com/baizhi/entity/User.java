c828be3cf9345dae798871cb7d8d356ac96da1c0
zhangcn <15010729294@163.com>
2020-12-30 11:21:25 +0800

    小黄：实现用户的基础功能

---
+++ src/main/java/com/baizhi/entity/User.java
@@ -0,0 +1,13 @@
+package com.baizhi.entity;
+
+/**
+ * @author User
+ * @time 2020/12/30-11:20
+ */
+public class User {
+
+    private String id;
+    private String name;
+    private Integer age;
+
+}
