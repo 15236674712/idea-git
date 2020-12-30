5cc9ef4f5ceef2d3b7a24cf28d1d5815cdbcd177
zhangcn <15010729294@163.com>
2020-12-30 11:01:16 +0800

    测试类修改代码2

--- src/test/java/com/baizhi/IdeaTests.java
+++ src/test/java/com/baizhi/IdeaTests.java
@@ -15,6 +15,9 @@ class IdeaTests {
     public static void main(String[] args) {
         int a = 20;
         System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
     }
 
     public void tests() {
