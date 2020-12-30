8806464dfc6cc1c80c96d6b3117763101220e4e2
zhangcn <15010729294@163.com>
2020-12-30 11:29:16 +0800

    小黄：userDao添加一个方法

--- src/main/java/com/baizhi/dao/UserDao.java
+++ src/main/java/com/baizhi/dao/UserDao.java
@@ -2,7 +2,11 @@ package com.baizhi.dao;
 
 import com.baizhi.entity.User;
 
+import java.util.List;
+
 public interface UserDao {
 
     User queryByUserId(String id);
+
+    List<User> queryAll();
 }
